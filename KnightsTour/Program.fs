d108c6d2193785d1c915d8fe0cb0738334b1bb2e
wwestlake <wwestlake@lagdaemon.com>
2016-11-12 12:27:07 -0500

    initial setup

--- KnightsTour/Program.fs
+++ KnightsTour/Program.fs
@@ -3,5 +3,10 @@
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "Knights Tour in F#"
+    printfn "By William Westlake (C)2016, All Rights Reserved"
+    printfn "Comments Suggestions?  wwestlake@lagdaemon.com"
+    printfn "All commands begin with :"
+    printfn ":help for help"
+
     0 // return an integer exit code
